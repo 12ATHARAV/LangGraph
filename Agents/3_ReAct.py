39e29e1f9504939121152feb145fa3fc94446e06
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-02-11 10:31:08 +0530

    update

--- Agents/3_ReAct.py
+++ Agents/3_ReAct.py
@@ -1,3 +1,46 @@
+# from typing import Annotated, Sequence, TypedDict
+# from dotenv import load_dotenv  
+# from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph
+# from langchain_core.messages import ToolMessage # Passes data back to LLM after it calls a tool such as the content and the tool_call_id
+# from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM
+# from langchain_groq import ChatGroq
+# from langchain_core.tools import tool
+# from langgraph.graph.message import add_messages
+# from langgraph.graph import StateGraph, END
+# from langgraph.prebuilt import ToolNode
+
+
+# load_dotenv()
+
+
+# # # Annotated - provides additional context without affecting the type itself
+# # email = Annotated[str, "This has to be a valid email format!"]
+# # print(email.__metadata__)
+
+
+# # Sequence -- To automatically handle the state updates for sequences such as by adding new messages to a chat history
+
+
+# # Reducer Function
+# # Rule that controls how updates from nodes are combined with the existing state.
+# # Tells us how to merge new data into the current state
+
+# # Without a reducer, updates would have replaced the existing value entirely!
+
+# # # Without a reducer
+# # state = {"messages": ["Hi!"]}
+# # update = {"messages": ["Nice to meet you!"]}
+# # new_state = {"messages": ["Nice to meet you!"]}
+
+# # # With a reducer
+# # state = {"messages": ["Hi!"]}
+# # update = {"messages": ["Nice to meet you!"]}
+# # new_state = {"messages": ["Hi!", "Nice to meet you!"]}
+
+
+
+
+
 from typing import Annotated, Sequence, TypedDict
 from dotenv import load_dotenv  
 from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph
@@ -12,56 +55,77 @@ from langgraph.prebuilt import ToolNode
 
 load_dotenv()
 
-
-# # Annotated - provides additional context without affecting the type itself
-# email = Annotated[str, "This has to be a valid email format!"]
-# print(email.__metadata__)
-
-
-# Sequence -- To automatically handle the state updates for sequences such as by adding new messages to a chat history
-
-
-# Reducer Function
-# Rule that controls how updates from nodes are combined with the existing state.
-# Tells us how to merge new data into the current state
-
-# Without a reducer, updates would have replaced the existing value entirely!
-
-# # Without a reducer
-# state = {"messages": ["Hi!"]}
-# update = {"messages": ["Nice to meet you!"]}
-# new_state = {"messages": ["Nice to meet you!"]}
-
-# # With a reducer
-# state = {"messages": ["Hi!"]}
-# update = {"messages": ["Nice to meet you!"]}
-# new_state = {"messages": ["Hi!", "Nice to meet you!"]}
-
-
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
 
+
 @tool
-def add(a: int, b: int):
-    """This is an addition function that adds 2 numer together"""
-    return a+b
-    
-tools = [add]
+def add(a: int, b:int):
+    """This is an addition function that adds 2 numbers together"""
+
+    return a + b 
+
+@tool
+def subtract(a: int, b: int):
+    """Subtraction function"""
+    return a - b
+
+@tool
+def multiply(a: int, b: int):
+    """Multiplication function"""
+    return a * b
+
+tools = [add, subtract, multiply]
 
 model = ChatGroq(model = "qwen/qwen3-32b").bind_tools(tools)
-    
+
+
 def model_call(state:AgentState) -> AgentState:
     system_prompt = SystemMessage(content=
-        "you are my AI assistant, please answer my query to best of your ability"                              
+        "You are my AI assistant, please answer my query to the best of your ability, in a precise way, without lengthening the response"
     )
-    response = model.invoke([system_prompt])
+    response = model.invoke([system_prompt] + state["messages"])
     return {"messages": [response]}
 
 
-def should_continue(state: AgentState):
+def should_continue(state: AgentState): 
     messages = state["messages"]
     last_message = messages[-1]
-    if not last_message.tool_calls:
+    if not last_message.tool_calls: 
         return "end"
     else:
         return "continue"
+    
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+
+tool_node = ToolNode(tools=tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END,
+    },
+)
+
+graph.add_edge("tools", "our_agent")
+
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+inputs = {"messages": [("user", "Add 40 + 12 and then multiply the result by 6. Also tell me a joke please.")]}
+print_stream(app.stream(inputs, stream_mode="values"))
