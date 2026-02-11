d7763d0b93ed67fd26f1053e9e4f2e2a66f75eb2
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-02-11 10:50:19 +0530

    update

--- Agents/3_ReAct.py
+++ Agents/3_ReAct.py
@@ -1,16 +1,13 @@
-# from typing import Annotated, Sequence, TypedDict
-# from dotenv import load_dotenv  
-# from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph
-# from langchain_core.messages import ToolMessage # Passes data back to LLM after it calls a tool such as the content and the tool_call_id
-# from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM
-# from langchain_groq import ChatGroq
-# from langchain_core.tools import tool
-# from langgraph.graph.message import add_messages
-# from langgraph.graph import StateGraph, END
-# from langgraph.prebuilt import ToolNode
-
-
-# load_dotenv()
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv  
+from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph
+from langchain_core.messages import ToolMessage # Passes data back to LLM after it calls a tool such as the content and the tool_call_id
+from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM
+from langchain_groq import ChatGroq
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
 
 
 # # # Annotated - provides additional context without affecting the type itself
@@ -37,20 +34,6 @@
 # # update = {"messages": ["Nice to meet you!"]}
 # # new_state = {"messages": ["Hi!", "Nice to meet you!"]}
 
-
-
-
-
-from typing import Annotated, Sequence, TypedDict
-from dotenv import load_dotenv  
-from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph
-from langchain_core.messages import ToolMessage # Passes data back to LLM after it calls a tool such as the content and the tool_call_id
-from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM
-from langchain_groq import ChatGroq
-from langchain_core.tools import tool
-from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph, END
-from langgraph.prebuilt import ToolNode
 
 
 load_dotenv()
