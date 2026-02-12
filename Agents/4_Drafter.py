5ee4753a625227a90f5073b1309e118a4b9a1a17
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-02-12 11:57:43 +0530

    update

--- Agents/4_Drafter.py
+++ Agents/4_Drafter.py
@@ -8,3 +8,79 @@ from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode
 
 load_dotenv()
+
+
+#this is the global variable to store document content
+document_content = ""
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+
+@tool
+def update(content: str) -> str:
+    """updates the document with the provided content."""
+    global document_content
+    document_content = content
+    return f"Document has been updated successfully! The current content is: \n{document_content}"
+
+
+@tool
+def save(filename: str) -> str:
+    """Save the current document to a text file and finish the process.
+    
+    Args:
+        filename: Name for the text file.
+    """
+
+    global document_content
+
+    if not filename.endswith('.txt'):
+        filename = f"{filename}.txt"
+
+
+    try:
+        with open(filename, 'w') as file:
+            file.write(document_content)
+        print(f"\n💾 Document has been saved to: {filename}")
+        return f"Document has been saved successfully to '{filename}'."
+    
+    except Exception as e:
+        return f"Error saving document: {str(e)}"
+    
+    
+    
+tools = [update, save]
+
+model = ChatGroq(model = "qwen/qwen3-32b").bind_tools(tools)
+
+
+def our_agent(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage(content=f"""
+    You are Drafter, a helpful writing assistant. You are going to help the user update and modify documents.
+    
+    - If the user wants to update or modify content, use the 'update' tool with the complete updated content.
+    - If the user wants to save and finish, you need to use the 'save' tool.
+    - Make sure to always show the current document state after modifications.
+    
+    The current document content is:{document_content}
+    """)
+
+    if not state["messages"]:
+        user_input = "I'm ready to help you update a document. What would you like to create?"
+        user_message = HumanMessage(content=user_input)
+
+    else:
+        user_input = input("\nWhat would you like to do with the document? ")
+        print(f"\n👤 USER: {user_input}")
+        user_message = HumanMessage(content=user_input)
+
+    all_messages = [system_prompt] + list(state["messages"]) + [user_message]
+
+    response = model.invoke(all_messages)
+
+    print(f"\n🤖 AI: {response.content}")
+    if hasattr(response, "tool_calls") and response.tool_calls:
+        print(f"🔧 USING TOOLS: {[tc['name'] for tc in response.tool_calls]}")
+
+    return {"messages": list(state["messages"]) + [user_message, response]}
